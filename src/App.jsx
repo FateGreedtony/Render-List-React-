20b0b989398232091eb55097f67d538e4a2c2bfe
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-07-30 15:55:19 +0700

    added render list for obj

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,59 @@
 function App() {
-  const keranjangBuah = ["Apel", "Jeruk", "Manggis", "Durian"];
+  const studentList = [
+    { id: 1, nama: "Bastian", kelas: "X PPLG 2" },
+    { id: 2, nama: "Fajar", kelas: "XI PPLG 3" },
+    { id: 3, nama: "Adit", kelas: "XII PPLG 1" }
+  ];
 
   return (
     <div style={{ 
-      padding: "1.7rem",
+      minHeight: "100vh",
       display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-      }}
-    >
+      justifyContent: "center"
+    }}>
 
-      <div style={{ fontSize: "1.5rem" }}>
-        <h2>Daftar Buah</h2>
+      <div style={{ fontSize: "2rem" }}>
+        <h2>Daftar Siswa</h2>
         <ul>
-          {keranjangBuah.map((buah) => (
-            <li>{buah}</li>
+          {studentList.map((siswa) => (
+            <li key={siswa.id}>
+              Nama: {siswa.nama} | Kelas: {siswa.kelas} | ID: {siswa.id}
+            </li>
           ))}
         </ul>
       </div>
-      
+
     </div>
   );
 }
 
 export default App;
+
+
+
+// function App() {
+//   const keranjangBuah = ["Apel", "Jeruk", "Manggis", "Durian"];
+
+//   return (
+//     <div style={{ 
+//       padding: "1.7rem",
+//       display: "flex",
+//       justifyContent: "center",
+//       alignItems: "center"
+//       }}
+//     >
+
+//       <div style={{ fontSize: "1.5rem" }}>
+//         <h2>Daftar Buah</h2>
+//         <ul>
+//           {keranjangBuah.map((buah) => (
+//             <li>{buah}</li>
+//           ))}
+//         </ul>
+//       </div>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
